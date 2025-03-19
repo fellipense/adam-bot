b8d7d26a2de9847106535d59b562ca1eb6edb131
fellipense <kingzinhu@hotmail.com>
2025-03-18 22:04:13 -0300

    giving "oi"

--- src/main.py
+++ src/main.py
@@ -6,12 +6,21 @@ import os
 
 load_dotenv()
 _token = os.getenv('TOKEN')
+intents = discord.Intents.all()
 
-bot = discord.Bot()
+bot = discord.Bot(command_prefix=".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot online!")
 
 @bot.command(description="Sends the bot's latency.")
 async def ping(ctx): 
-    await ctx.respond(f"Pong! Latency is {bot.latency}")
+    await ctx.respond(f"Pong! Latency is `{bot.latency:.3f}`s")
 
+@bot.event
+async def on_message(ctx):
+    if ctx.author.bot: return
+    if ctx.content == "oi": await ctx.reply("oi")
 
 bot.run(_token)
